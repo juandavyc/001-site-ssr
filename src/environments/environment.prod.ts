b524617e669e9dc2079314b3dc8890256e5fd180
juandavyc <davidy0090011@gmail.com>
2024-12-17 19:47:07 -0500

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   GIT_URL_PROFILE: 'https://api.github.com/users',
   GIT_URL_REPOSITORIES: 'https://api.github.com/search/repositories',
   GALLERY_URL: 'data/test/gallery',
-  token: ''
+  token: 'no-token-need'
 }
